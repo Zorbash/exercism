83051d7b9cdc6d5a1213e1d7a43334f1d36c24de
Dimitris Zorbas <zorbash@skroutz.gr>
2015-08-23 14:02:07 +0300

    [ruby] Add hamming solution iteration 1

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,16 @@
+module Hamming
+  extend self
+
+  VERSION = 1
+
+  def compute(stra, strb)
+    raise ArgumentError unless stra.size == strb.size
+
+    strb.chars.tap do |strb_chars|
+      return stra.chars.each.with_index.reduce(0) do |dist, (char, idx)|
+        dist += 1 if char != strb_chars[idx]
+        dist
+      end
+    end
+  end
+end
